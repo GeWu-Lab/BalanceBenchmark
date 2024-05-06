ed8af53e1a5eccffc037e5ecbd18a6adfc3f03a4
Shaoxuan Xu <2799449627xu@gmail.com>
2024-05-07 02:38:54 +0800

    food_dataset update

--- balancemm/utils/data_utils.py
+++ balancemm/utils/data_utils.py
@@ -3,6 +3,12 @@ from torchvision import datasets, transforms
 from types import SimpleNamespace
 from ..datasets import create_dataset
 import lightning as L
+import numpy as np
+import random
+def worker_init_fn(worker_id):
+    worker_seed = torch.initial_seed() % 2**32
+    np.random.seed(worker_seed)
+    random.seed(worker_seed)
 
 def create_train_val_dataloader(fabric: L.Fabric, config: dict):
     config = SimpleNamespace(**config)
@@ -14,7 +20,6 @@ def create_train_val_dataloader(fabric: L.Fabric, config: dict):
     if config_dataloader.fast_run == True:
         train_dataset = torch.utils.data.Subset(train_dataset, list(range(config_dataloader.eff_batch_size*4)))
         val_dataset = torch.utils.data.Subset(val_dataset, list(range(config_dataloader.eff_batch_size*2)))
-    
     # print len of datasets
     fabric.print(f"Train dataset: {train_dataset.__class__.__name__} - {config.train['name']}, {len(train_dataset)} samples")
     fabric.print(f"Val dataset: {val_dataset.__class__.__name__} - {config.val['name']}, {len(val_dataset)} samples")
@@ -22,10 +27,37 @@ def create_train_val_dataloader(fabric: L.Fabric, config: dict):
     if (not hasattr(config_dataloader, 'batch_size')):
         config_dataloader.batch_size = round(config_dataloader.eff_batch_size/fabric.world_size) # using the effective batch_size to calculate the batch_size per gpu
     
-    train_dataloader = torch.utils.data.DataLoader(train_dataset,  batch_size=config_dataloader.batch_size, shuffle=config_dataloader.shuffle, drop_last = config_dataloader.drop_last, num_workers = config_dataloader.num_workers, multiprocessing_context='spawn', pin_memory = config_dataloader.pin_memory)
+    train_dataloader = torch.utils.data.DataLoader(train_dataset,  
+                                                   batch_size=config_dataloader.batch_size, 
+                                                   shuffle=config_dataloader.shuffle, 
+                                                   drop_last = config_dataloader.drop_last,
+                                                     num_workers = config_dataloader.num_workers, 
+                                                     multiprocessing_context='spawn', 
+                                                     pin_memory = config_dataloader.pin_memory)
 
     val_dataloader = torch.utils.data.DataLoader(val_dataset,  batch_size=config_dataloader.batch_size, drop_last = config_dataloader.drop_last, num_workers = config_dataloader.num_workers, multiprocessing_context='spawn', pin_memory = config_dataloader.pin_memory)
 
+    if config.train['name'] == 'UMPC_Food':
+        g = torch.Generator()
+        train_dataloader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=config_dataloader.batch_size, 
+                                                shuffle=config_dataloader.shuffle, 
+                                                drop_last = config_dataloader.drop_last,
+                                                num_workers = config_dataloader.num_workers, 
+                                                multiprocessing_context='spawn', 
+                                                pin_memory = config_dataloader.pin_memory,
+                                               worker_init_fn=worker_init_fn,
+                                               generator=g)
+
+        val_dataloader = torch.utils.data.DataLoader(val_dataset,
+                                                batch_size=config_dataloader.batch_size, 
+                                                shuffle=config_dataloader.shuffle, 
+                                                drop_last = config_dataloader.drop_last,
+                                                num_workers = config_dataloader.num_workers, 
+                                                multiprocessing_context='spawn', 
+                                                pin_memory = config_dataloader.pin_memory,
+                                                worker_init_fn=worker_init_fn,
+                                                generator=g)
     # print batchsize and len
     fabric.print(f"Train dataloader: {len(train_dataloader)} batches, {len(train_dataloader.dataset)} samples")
     fabric.print(f"Val dataloader: {len(val_dataloader)} batches, {len(val_dataloader.dataset)} samples")
