0c2022755fbcf620c49ae717cd508a5e1cdd8dbd
Shaoxuan Xu <2799449627xu@gmail.com>
2024-05-07 02:38:27 +0800

    VTclassifier update

--- balancemm/models/avclassify_model.py
+++ balancemm/models/avclassify_model.py
@@ -76,6 +76,7 @@ class Transformer(nn.Module):
         """
         if type(x) is list:
             x = x[0]
+        # x = x.to(self.embedding.weight.dtype)
         x = self.embedding(x)
         x = self.conv(x.permute([0,2,1]))
         x = x.permute([2,0,1])
@@ -97,7 +98,7 @@ class TransformerEncoder(nn.Module):
 
         self.activation = nn.ReLU()
 
-    def forward(self, src, src_mask=None, src_key_padding_mask=None):
+    def forward(self, src,is_causal, src_mask=None, src_key_padding_mask=None):
         # 自注意力层
         src2 = self.self_attn(src, src, src, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
@@ -151,6 +152,9 @@ class AVTClassifierModel(nn.Module):
         visual = batch['visual']
         audio = batch['audio']
         text = batch['text']
+        print(visual.shape)
+        print(audio)
+        print(text)
         visual = visual.to(self.device)
         audio = audio.to(self.device)
         text = text.to(self.device)
@@ -447,3 +451,77 @@ class AVTClassifier_gbModel(AVTClassifierModel):
             if np.asarray(label[i].cpu()) == t:
                 acc_t[label[i]] += 1.0
         return loss, sum(acc), sum(acc_a), sum(acc_v), sum(acc_t)
+    
+class VTClassifierModel(AVTClassifierModel):
+    def __init__(self, args):
+        super(VTClassifierModel, self).__init__(args)
+        #### audio means text
+        n_classes = args['n_classes']
+        fusion = args['fusion']
+
+        self.n_classes = n_classes
+        # self.audio_net = ResNet18(modality='audio')
+        self.visual_net = ResNet18(modality='visual')
+        # self.visual_net = Transformer(input_dim = 35)
+        self.audio_net = Transformer(input_dim=40, dim=512)
+        self.fusion = fusion
+        self.device = None
+        if fusion == 'sum':
+            self.fusion_module = SumFusion(output_dim=n_classes)
+        elif fusion == 'concat':
+            self.fusion_module = ConcatFusion(output_dim=n_classes, input_dim=1024)
+        elif fusion == 'film':
+            self.fusion_module = FiLM(output_dim=n_classes, x_film=True)
+        elif fusion == 'gated':
+            self.fusion_module = GatedFusion(output_dim=n_classes, x_gate=True)
+        else:
+            raise NotImplementedError('Incorrect fusion method: {}!'.format(fusion))
+        
+    def forward(self,
+                batch,
+                pad_audio = False,
+                pad_visual = False, 
+                pad_text = False, 
+                mask = None, 
+                dependent_modality = {"audio": False, "visual": False, "text": False}, 
+                pt = 0,
+                types= 0):
+        visual = batch['visual']
+        audio = batch['audio']
+        visual = visual.to(self.device)
+        audio = audio.to(self.device)
+        if pad_audio:
+            audio = torch.zeros_like(audio, device=audio.device)
+        if pad_visual:
+            visual = torch.zeros_like(visual, device=visual.device)
+        visual = visual.permute(0, 2, 1, 3, 4).contiguous().float()
+        audio = audio.unsqueeze(1).float()
+
+        a = self.audio_net(audio)
+        v = self.visual_net(visual)
+        (_, C, H, W) = v.size()
+        B = a.size()[0]
+        v = v.view(B, -1, C, H, W)
+        v = v.permute(0, 2, 1, 3, 4)
+
+        # a = F.adaptive_avg_pool2d(a, 1)
+        v = F.adaptive_avg_pool3d(v, 1)
+
+        a = torch.flatten(a, 1)
+        v = torch.flatten(v, 1)
+
+        if dependent_modality['audio']:
+            a = torch.mul(a,mask)
+            if(abs(pt-1)>0.1):
+                a = a*1/(1-pt)
+            else:
+                a = a*10
+        elif dependent_modality['visual']:
+            v = torch.mul(v,mask)
+            if(abs(pt-1)>0.1):
+                v = v*1/(1-pt)
+            else:
+                v = v*10
+
+        a, v, out = self.fusion_module(a, v)
+        return a, v, out
