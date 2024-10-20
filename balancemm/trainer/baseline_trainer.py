400d260f61aa1c79e695271e2a4e8e5971860648
Louise158 <Louise158@stu.sufe.edu.cn>
2024-10-20 16:55:20 +0800

    add KSDataset config

--- balancemm/trainer/baseline_trainer.py
+++ balancemm/trainer/baseline_trainer.py
@@ -15,7 +15,6 @@ from typing import Any, Iterable, List, Literal, Optional, Tuple, Union, cast
 import lightning as L
 import torch
 from ..models.avclassify_model import BaseClassifierModel
-from ..evaluation.complex import get_flops
 class baselineTrainer(BaseTrainer):
     def __init__(self,fabric, method_dict: dict = {}, para_dict : dict = {}):
         super(baselineTrainer,self).__init__(fabric,**para_dict)
@@ -54,14 +53,7 @@ class baselineTrainer(BaseTrainer):
         iterable = self.progbar_wrapper(
             train_loader, total=min(len(train_loader), limit_batches), desc=f"Epoch {self.current_epoch}"
         )
-        if self.current_epoch == 0:
-            for batch_idx, batch in enumerate(iterable):
-                batch_sample = batch
-                break
-            print(batch_sample.keys())
-            model_flops, _ =get_flops(model = model, input_sample = batch_sample)
-            self.FlopsMonitor.update(model_flops / len(batch_sample['label']) * len(train_loader), 'forward')
-            self.FlopsMonitor.report(logger = self.logger)
+
         for batch_idx, batch in enumerate(iterable):
             # end epoch if stopping training completely or max batches for this epoch reached
             if self.should_stop or batch_idx >= limit_batches:
@@ -110,7 +102,7 @@ class baselineTrainer(BaseTrainer):
         #     a, v, out = model(batch)
         # else:
         #     a, v, t, out = model(batch)
-        _ = model(batch= batch)
+        _ = model.validation_step(batch= batch, batch_idx= batch_idx, limit_modality = model.modalitys)
         out = model.encoder_res['output']        
         loss = criterion(out, label)
         loss.backward()
